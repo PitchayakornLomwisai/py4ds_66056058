1d0f6dfd5a82e16bf91edfb8da8d5b24d072ea82
peacechyk <95404776+peacechyk@users.noreply.github.com>
2023-10-13 16:17:26 +0700

    FIX : bottles_of_beer function ex23

--- src/ex23.py
+++ src/ex23.py
@@ -3,7 +3,7 @@ Exercise 23 : 99 Bottles of Beer on the Wall
 """
 
 
-def bottles_of_beer(param):
+def bottles_of_beer(num_bottles):
     """
     Calculate the number of bottles of beer on the wall.
     And print out like this :
@@ -22,13 +22,16 @@ def bottles_of_beer(param):
         str: The repeated lyrics of bottles of beer on the wall.
     """
     # TODO : complete this
-    pass
+    if num_bottles == 1:
+        msg = "1 bottle of beer on the wall,\n1 bottle of beer.\nTake one down, pass it around,\nNo more bottles of beer on the wall!\n"
+    else:
+        msg = f"{num_bottles} bottles of beer on the wall,\n{num_bottles} bottles of beer.\nTake one down, pass it around,\n{num_bottles-1} bottles of beer on the wall.\n"
+    return msg
 
-
-def loop_bottles_of_bears(bottle):
-    while bottle > 0:
-        print(bottles_of_beer(bottle))
-        bottle -= 1
+def loop_bottles_of_bears(bottles):
+    while bottles > 0:
+        print(bottles_of_beer(bottles))
+        bottles -= 1
 
 
 if __name__ == '__main__':
