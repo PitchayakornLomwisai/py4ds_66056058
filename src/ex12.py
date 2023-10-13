0d5c5c7025faf54b17589b5f9cd163c383028e14
peacechyk <95404776+peacechyk@users.noreply.github.com>
2023-10-13 15:01:26 +0700

    FIX : get_smallest function ex12

--- src/ex12.py
+++ src/ex12.py
@@ -3,7 +3,7 @@ Execise 12
 """
 
 
-def get_smallest(param):
+def get_smallest(lst):
     """
     Get the smallest number from a list of numbers.
 
@@ -13,4 +13,7 @@ def get_smallest(param):
     Returns:
         int or None: The smallest number from the list. If the list is empty, returns None.
     """
-    pass
+    if len(lst) == 0:
+        return None
+    else:
+        return min(lst)
