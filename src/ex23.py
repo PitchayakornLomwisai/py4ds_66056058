da5b0972b1da56fa0a9ea4f8a628fc374fc0a7c3
peacechyk <95404776+peacechyk@users.noreply.github.com>
2023-10-13 16:17:37 +0700

    FIX : bottles_of_beer function ex23

--- src/ex23.py
+++ src/ex23.py
@@ -21,7 +21,7 @@ def bottles_of_beer(num_bottles):
     Returns:
         str: The repeated lyrics of bottles of beer on the wall.
     """
-    # TODO : complete this
+    # FIX : complete this
     if num_bottles == 1:
         msg = "1 bottle of beer on the wall,\n1 bottle of beer.\nTake one down, pass it around,\nNo more bottles of beer on the wall!\n"
     else:
