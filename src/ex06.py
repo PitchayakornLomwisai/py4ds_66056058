4c7ae668a83623f5e94a90496df75bfdc117548b
peacechyk <95404776+peacechyk@users.noreply.github.com>
2023-10-13 14:24:34 +0700

    FIX : ordinal_suffix function ex06

--- src/ex06.py
+++ src/ex06.py
@@ -3,7 +3,7 @@ Execise 6
 """
 
 
-def ordinal_suffix(param):
+def ordinal_suffix(num):
     """
     Return the ordinal suffix for a given number.
 
@@ -14,4 +14,16 @@ def ordinal_suffix(param):
         str: The ordinal suffix corresponding to the given number.
     """
     # TODO : complete this
-    pass
+    if 10 <= num % 100 <= 20:
+        suffix = "th"
+    else:
+        last_digit = num % 10
+        if last_digit == 1:
+            suffix = "st"
+        elif last_digit == 2:
+            suffix = "nd"
+        elif last_digit == 3:
+            suffix = "rd"
+        else:
+            suffix = "th"
+    return str(num) + suffix
