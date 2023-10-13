6109a04e035868a2083658c063d85278fe28809e
peacechyk <95404776+peacechyk@users.noreply.github.com>
2023-10-13 15:33:10 +0700

    FIX : get_cost_of_coffee and get_cost_of_coffee_2 function ex18

--- src/ex18.py
+++ src/ex18.py
@@ -3,5 +3,54 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
-    pass
+def get_cost_of_coffee(num_coffees, price_per_coffee):
+    """
+    Calculate the total cost of buying a number of coffees with the "Buy 8 get 1 free" promotion.
+
+    Args:
+        num_coffees (int): The total number of coffees to buy.
+        price_per_coffee (float): The cost of one cup of coffee.
+
+    Returns:
+        float: The total cost of buying the given number of coffees with the promotion.
+    """
+    cost = 0.0
+    cups_until_free_coffee = 8
+
+    while num_coffees > 0:
+        if cups_until_free_coffee == 0:
+            cups_until_free_coffee = 8
+            num_coffees -= 1
+        else:
+            cost += price_per_coffee
+            cups_until_free_coffee -= 1
+            num_coffees -= 1
+
+    return round(cost, 2)
+
+def get_cost_of_coffee_2(num_coffees, price_per_coffee):
+    """
+    Calculate the total cost of buying a number of coffees with the "Buy 8 get 1 free" promotion.
+
+    Args:
+        num_coffees (int): The total number of coffees to buy.
+        price_per_coffee (float): The cost of one cup of coffee.
+
+    Returns:
+        float: The total cost of buying the given number of coffees with the promotion.
+    """
+    cost = 0.0
+    cups_until_free_coffee = 8
+
+    while num_coffees > 0:
+        if cups_until_free_coffee == 0:
+            cups_until_free_coffee = 8
+            num_coffees -= 1
+        else:
+            cost += price_per_coffee
+            cups_until_free_coffee -= 1
+            num_coffees -= 1
+
+    return round(cost, 2)
+
+
