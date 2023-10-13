6b5104d25c8871a838cd1a0dd9d5110ce226b884
peacechyk <95404776+peacechyk@users.noreply.github.com>
2023-10-13 14:20:16 +0700

    FIX : fizz_buzz function ex05

--- src/ex05.py
+++ src/ex05.py
@@ -3,7 +3,7 @@ Execise 5
 """
 
 
-def fizz_buzz(param):
+def fizz_buzz(num):
     """
     Checks if the given number is divisible by 3 and 5. If it is, returns "FizzBuzz".
     If the number is only divisible by 3, returns "Fizz".
@@ -16,5 +16,12 @@ def fizz_buzz(param):
     Returns:
         str or int: The result of the FizzBuzz calculation.
     """
-    # TODO : complete this
-    pass
+    # FIX : complete this
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
