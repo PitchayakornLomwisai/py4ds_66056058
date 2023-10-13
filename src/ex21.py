56240849e94bb0e72376ec2456bdee94eeba600a
peacechyk <95404776+peacechyk@users.noreply.github.com>
2023-10-13 15:58:01 +0700

    FIX : is_valid_date function ex21

--- src/ex21.py
+++ src/ex21.py
@@ -3,7 +3,7 @@ Exercise 21 : Validate Date
 """
 
 
-def is_valid_date(param1, param2, param3):
+def is_valid_date(year, month, day):
     """
     Check if a given date is valid.
 
@@ -16,7 +16,15 @@ def is_valid_date(param1, param2, param3):
         bool: True if the date is valid, False otherwise.
     """
     # important to import here
-    from src.sol20 import is_leap_year
+    from src.ex20 import is_leap_year
 
-    # TODO : complete this
-    pass
+    # FIX : complete this
+    if (year < 1 or month < 1 or month > 12 or day < 1):
+        return False
+
+    max_days_per_month = [31, 28 if not is_leap_year(year) else 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if day > max_days_per_month[month - 1]:
+        return False
+
+    return True
